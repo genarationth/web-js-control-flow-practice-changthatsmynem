1d81e68c6f76e840e2b0e7a86b7558ca90520e4c
changthatsmynem <nathapab.jirasaweksil@gmail.com>
2023-03-13 13:59:46 +0700

    Day/Week Matching Number Complete

--- a-day-of-the-week-translator.js
+++ a-day-of-the-week-translator.js
@@ -1,19 +1,31 @@
-let dayNumber = Math.floor(Math.random() * 7);
+let dayNumber = Math.floor(Math.random() * 14);
+let dayMatchNum = "";
 
-if (dayNumber === 0) {
-  console.log("Sunday");
-} else if (dayNumber === 1) {
-  console.log("Monday");
-} else if (dayNumber === 2) {
-  console.log("Tuesday");
-} else if (dayNumber === 3) {
-  console.log("Wednesday");
-} else if (dayNumber === 4) {
-  console.log("Thursday");
-} else if (dayNumber === 5) {
-  console.log("Friday");
-} else if (dayNumber === 6) {
-  console.log("Saturday");
-} else {
-  console.log("Invalid Day Number");
+switch (dayNumber) {
+  case 0:
+    dayMatchNum = "Sunday";
+    break;
+  case 1:
+    dayMatchNum = "Monday";
+    break;
+  case 2:
+    dayMatchNum = "Tuesday";
+    break;
+  case 3:
+    dayMatchNum = "Wednesday";
+    break;
+  case 4:
+    dayMatchNum = "Thursday";
+    break;
+  case 5:
+    dayMatchNum = "Friday";
+    break;
+  case 6:
+    dayMatchNum = "Saturday";
+    break;
+  default:
+    dayMatchNum = "Invalid Day Number";
+    break;
 }
+
+console.log(dayMatchNum);
